5c542349cce76af4edfed51ceb65fa436041243a
Anirudh <icyph0x@pm.me>
2018-11-05 22:15:54 +0530

    Init
    Signed-off-by: Anirudh <icyph0x@pm.me>

---
+++ src/sup.nim
@@ -0,0 +1,24 @@
+import
+  nanoid,
+  os,
+  ospaths,
+  osproc,
+  strformat,
+  parsecfg
+
+try:
+  let 
+    dict = loadConfig(fmt"{getHomeDir()}.sup.conf")
+    host = dict.getSectionValue("", "hostname")
+    path = dict.getSectionValue("", "remotepath")
+    file = paramStr(1)
+    name = generate(size=5)
+
+  var (dir, f, ext) = splitFile(file)
+  let cmd = fmt"scp {file} {host}:{path}/{name}{ext}"
+  discard execCmd(cmd)
+  echo(fmt"done! file is at {host}:{path}/{name}{ext}")
+
+except IOError:
+  echo("error: config file not found")
+  quit(0)
