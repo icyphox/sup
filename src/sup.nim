d4f3fe8fc9ad6674ac8c99966875933ca417e3ec
Anirudh <icyph0x@pm.me>
2018-11-06 13:51:45 +0530

    Better error handling, output URL instead
    Signed-off-by: Anirudh <icyph0x@pm.me>

--- src/sup.nim
+++ src/sup.nim
@@ -5,19 +5,27 @@ import
   strformat,
   parsecfg
 
+var file = ""
+
+try:
+  file = paramStr(1)
+except IndexError:
+  echo("error: no file specified")
+  quit(0)
+
 try:
   let 
     dict = loadConfig(fmt"{getHomeDir()}.sup.conf")
     host = dict.getSectionValue("", "hostname")
     path = dict.getSectionValue("", "remotepath")
-    file = paramStr(1)
+    url  = dict.getSectionValue("", "url")
     name = generate(size=5)
-
   var (dir, f, ext) = splitFile(file)
   let cmd = fmt"scp {file} {host}:{path}/{name}{ext}"
   discard execCmd(cmd)
-  echo(fmt"done! file is at {host}:{path}/{name}{ext}")
-
+  echo(fmt"done! file is at {url}/{name}{ext}")
 except IOError:
   echo("error: config file not found")
   quit(0)
+except IndexError:
+  echo("error: config file has missing field(s)")
